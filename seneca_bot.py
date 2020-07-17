275c251be689b56d44b02d676a09c6de2d312dc5
Raihan9797 <raihanmuhd97@gmail.com>
2020-07-17 13:50:43 +0800

    used name == main

--- seneca_bot.py
+++ seneca_bot.py
@@ -2,43 +2,17 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, PicklePersiste
 from bot_token import token
 TOKEN = token
 
-### creating the updater and dispatcher
-from telegram.ext import Updater
-updater = Updater(token = token, persistence=my_persistence, use_context=True)
-dispatcher = updater.dispatcher
-
-
 ### logging to see errors
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 ########## start function ##########
-
-## storing list of letters and dictionary into the bot_data
-fn = 'meta_letters/Volume 1.txt'
-with open(fn, 'r') as fo:
-    vol1 = fo.read()
-fn = 'meta_letters/Volume 2.txt'
-with open(fn, 'r') as fo:
-    vol2 = fo.read()
-
-# fn = 'all_letters/dict_0to10.json'
-fn = 'all_letters/dict_all_letters.json'
-with open(fn, 'r') as fo:
-    import json
-    letters = json.load(fo)
-
 def start(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = "Starting! \nType '/help' to know the commands!")
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "TEST MODE! \nType '/help' to know the commands!")
     # store data
-    context.bot_data['vol 1'] = vol1
-    context.bot_data['vol 2'] = vol2
-    context.bot_data['letters'] = letters
-
-
 start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+
 
 ######### list letter names function #########
 def list_letters(update, context):
@@ -46,7 +20,7 @@ def list_letters(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = str(context.bot_data['vol 2']) )
 
 list_handler = CommandHandler('list', list_letters)
-dispatcher.add_handler(list_handler)
+
 
 ####### read a letter function #########
 def read(update, context):
@@ -61,19 +35,55 @@ def read(update, context):
         update.message.reply_text("no such letter found")
 
 read_handler = CommandHandler('read',read)
-dispatcher.add_handler(read_handler)
 
+
+####### help: lists commands #######
 def help(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "/list : list all the letters \n/read <letter_number>: fetches selected letter")
 help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
 
-### HEROKU PORTS
-import os
-PORT = int(os.environ.get('PORT', '8443'))
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://senecabot-01.herokuapp.com/" + TOKEN)
-updater.idle()
+
+
+####### MAIN FUNCTION ie setup ######
+def main():
+    ### creating the updater and dispatcher
+    from telegram.ext import Updater
+    updater = Updater(token = token, persistence=my_persistence, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    ## storing list of letters and dictionary into the bot_data
+    fn = 'meta_letters/Volume 1.txt'
+    with open(fn, 'r') as fo:
+        vol1 = fo.read()
+    fn = 'meta_letters/Volume 2.txt'
+    with open(fn, 'r') as fo:
+        vol2 = fo.read()
+    # fn = 'all_letters/dict_0to10.json'
+    fn = 'all_letters/dict_all_letters.json'
+    with open(fn, 'r') as fo:
+        import json
+        letters = json.load(fo)
+    
+    #### storing into bot_data
+    dispatcher.bot_data['vol 1'] = vol1
+    dispatcher.bot_data['vol 2'] = vol2
+    dispatcher.bot_data['letters'] = letters
+
+    #### adding handlers
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(read_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(list_handler)
+
+    ### HEROKU PORTS
+    import os
+    PORT = int(os.environ.get('PORT', '8443'))
+    # add handlers
+    updater.start_webhook(listen="0.0.0.0",
+                        port=PORT,
+                        url_path=TOKEN)
+    updater.bot.set_webhook("https://senecabot-01.herokuapp.com/" + TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
